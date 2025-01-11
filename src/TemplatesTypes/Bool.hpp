b3523fd3fed730ec34fe5427e595bbe48f85f373
vugsk <ganchukovnikita@bk.ru>
2025-01-11 16:11:41 +0700

    operators add in class Float

---
+++ src/TemplatesTypes/Bool.hpp
@@ -0,0 +1,19 @@
+//
+// Created by nikita on 11.01.25.
+//
+
+#ifndef BOOL_HPP
+#define BOOL_HPP
+
+
+namespace types
+{
+
+class Bool
+{
+
+};
+
+}
+
+#endif //BOOL_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,18 @@
 
 #include "Types.hpp"
 
-
 int main()
 {
+    constexpr int32 i32{32};
 
-    int32 i32{32};
-
-    float32 f32{90};
-    float64 f64{i32};
-    float128 f128{8.8};
+    constexpr float32 f32{90};
+    const float64 f64{i32};
+    constexpr float128 f128{8.8};
 
     std::cout << f32 << ' ' << f64 << ' ' << f128 << '\n';
-    std::cout << std::numeric_limits<long double>::max() << '\n';
-    std::cout << static_cast<long double>(pow(2, 32*4*16));
+    std::cout << (f32 >= f32) << '\n' << (f64 > 3) << '\n';
+    std::cout << float32::max() << ' ' << float32::min() << '\n';
+    std::cout << static_cast<long double>(pow(2, 32*16));
 
     return 0;
 }
