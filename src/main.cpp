6ef9ab8f19bc9a64089577bc932dd6d7182101c0
vugsk <ganchukovnikita@bk.ru>
2025-01-11 20:51:31 +0700

    create class Bool

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,16 @@
 
 int main()
 {
-    constexpr int32 i32{32};
-
-    constexpr float32 f32{90};
-    const float64 f64{i32};
-    constexpr float128 f128{8.8};
-
-    std::cout << f32 << ' ' << f64 << ' ' << f128 << '\n';
-    std::cout << (f32 >= f32) << '\n' << (f64 > 3) << '\n';
-    std::cout << float32::max() << ' ' << float32::min() << '\n';
-    std::cout << static_cast<long double>(pow(2, 32*16));
+    // constexpr int32 i32{32};
+    //
+    // constexpr float32 f32{90};
+    // const float64 f64{i32};
+    // constexpr float128 f128{8.8};
+    //
+    // std::cout << f32 << ' ' << f64 << ' ' << f128 << '\n';
+    // std::cout << (f32 >= f32) << '\n' << (f64 > 3) << '\n';
+    // std::cout << float32::max() << ' ' << float32::min() << '\n';
+    // std::cout << static_cast<long double>(pow(2, 32*16));
 
     return 0;
 }
