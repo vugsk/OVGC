94f0298587965425ecafc30c380f72cd3b9e23ef
vugsk <ganchukovnikita@bk.ru>
2025-01-12 18:05:36 +0700

    fix bug at operations any types, for example, float / int32

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main()
     float64 f64{i32};
     float128 f128{8.8};
 
-    f32 /= f128;
+    f32 /= i32;
     std::cout << f32 + f64 << std::endl;
 
     // std::cout << static_cast<long double>(pow(2, 32*16));
