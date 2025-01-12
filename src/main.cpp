6d6aadc687e4121b14905d37799d8611933e33e5
vugsk <ganchukovnikita@bk.ru>
2025-01-12 23:37:41 +0700

    add operator equals and not equals in class Int

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,9 @@ int main()
 {
     setlocale(LC_ALL, "");
 
-    static constexpr int32 i32{89};
-    static constexpr float32 f32{90};
+    int16 i16;
 
-    char8 c8(i32);
-    char16 c16(f32);
-    char32 c32(L'р');
-    char16 ch16(L'л');
-    c8 = ch16;
-    std::wcout << ch16 << c32 << std::endl;
-
-    // std::cout << static_cast<long double>(pow(2, 32*16));
+    std::cout << i16 << std::endl;
 
     return 0;
 }
