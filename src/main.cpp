c0cd2afc5d6006c51fca45a986ce8387def68fc3
vugsk <ganchukovnikita@bk.ru>
2025-01-24 17:55:15 +0700

    add test

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,39 @@
 #include "BaseFunctionsObjects.hpp"
 #include "Types.hpp"
 
-
 int main()
 {
     setlocale(LC_ALL, "");
 
-    int32 i16{90};
-    float32 i8{0};
-    char8 ch{'A'};
-    bool1 bl{true};
+    //types::Pointer<int16> int_ptr{int32{45}};
+    //std::cout << *int_ptr << std::endl;
+
+    //types::Pointer<int16> i{12};
+
+    //types::Pointer<int32> j{};
+
+    //int32 ui{*j};
+
+    //j = static_cast<types::Pointer<int32>>(i);
+    // std::cout << *j << ' ' << (j == ui) << std::endl;
+
+    // int32 i16{90};
+    // float32 i8{0};
+    // char8 ch{'A'};
+    // bool1 bl{true};
+    //
+    // print(L"{} {} {} {}", i16, i8, ch, bl);
+
+    // types::String<char8> string;
+    //
+    // auto ch8_ptr = new char8[2];
+    // ch8_ptr[0] = 'l';
+    // ch8_ptr[1] = '\0';
+    // print("{}\n", ch8_ptr);
+    // delete[] ch8_ptr;
 
 
-    print(L"{} {} {} {}", i16, i8, ch, bl);
+
 
     return 0;
 }
